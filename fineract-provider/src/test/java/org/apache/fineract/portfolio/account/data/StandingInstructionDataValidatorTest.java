40c75fb20b30398faaaf53c2f1c2aebe197ee358
Francisco Cuandon <francisco.cuandon@fintecheando.mx>
2026-05-14 17:40:19 -0600

    Update calling method

--- fineract-provider/src/test/java/org/apache/fineract/portfolio/account/data/StandingInstructionDataValidatorTest.java
+++ fineract-provider/src/test/java/org/apache/fineract/portfolio/account/data/StandingInstructionDataValidatorTest.java
@@ -99,7 +99,7 @@ public class StandingInstructionDataValidatorTest {
 
             @Test
             void throwErrorWhitTransferTypeInvalidValue() {
-                assertRange(getBaseRequest()
+                assertRange(getBaseRequest(),
                     AccountDetailConstants.transferTypeParamName);
             }
 
